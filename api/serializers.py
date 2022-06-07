a598bcc0d44bd6b9b06886e8c0513a022488667b
siamzaagsyscom <siam@zaagsys.com>
2022-06-07 12:27:52 +0600

    Addie Soft Interview task complete withe dajngo rest

--- api/serializers.py
+++ api/serializers.py
@@ -16,6 +16,7 @@ class SalesSerializer(serializers.ModelSerializer):
     class Meta:
         model = Sales
         fields = "__all__"
+        
 
 class ProductSerializer(serializers.ModelSerializer):
     productby = SalesDetailsSerializer(read_only=True,many=True)
